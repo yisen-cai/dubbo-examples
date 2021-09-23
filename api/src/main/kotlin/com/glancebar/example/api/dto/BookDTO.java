6e000bee8cb8631ee74b255c9fd908f52649febe
YISEN CAI <yisen614@gmail.com>
2021-09-23 16:22:54 +0800

    修正Kotlin错误

---
+++ api/src/main/kotlin/com/glancebar/example/api/dto/BookDTO.java
@@ -0,0 +1,34 @@
+package com.glancebar.example.api.dto;
+
+import java.io.Serializable;
+
+/**
+ * 出参入参必须实现Serializable接口，Kotlin不支持
+ *
+ * @author YISHEN CAI
+ */
+public class BookDTO implements Serializable {
+    private String name;
+    private String isbn;
+
+    public BookDTO(String name, String isbn) {
+        this.name = name;
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
---
+++ provider/src/main/kotlin/com/glancebar/example/provider/config/DubboConfig.kt
@@ -0,0 +1,31 @@
+package com.glancebar.example.provider.config
+
+import org.apache.dubbo.config.ApplicationConfig
+import org.apache.dubbo.config.ProtocolConfig
+import org.apache.dubbo.config.RegistryConfig
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+/**
+ * 后续添加在yaml配置文件中自动生成Bean组件，而非配置在xml中
+ * @author YISHEN CAI
+ */
+@Configuration
+class DubboConfig {
+
+//    @Bean
+//    fun dubboRegistry(): RegistryConfig {
+//        TODO()
+//    }
+//
+//    @Bean
+//    fun dubboProtocol(): ProtocolConfig {
+//        TODO()
+//    }
+//
+//    @Bean
+//    fun dubboApplication(): ApplicationConfig {
+//        TODO()
+//    }
+}
